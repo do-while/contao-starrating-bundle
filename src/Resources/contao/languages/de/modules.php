90a7428d8a25321ca3a09f0c497058534ff4b2bc
Sven Rhinow <sven@sr-tag.de>
2019-03-05 13:12:41 +0100

    add Backend-Modul-Übersetzung

---
+++ src/Resources/contao/languages/de/modules.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+ * Backend modules
+ */
+$GLOBALS['TL_LANG']['MOD']['starrating'][0] = 'Sterne-Bewertung';
+$GLOBALS['TL_LANG']['MOD']['starrating_pages'][0] = 'Bewertungen';
+$GLOBALS['TL_LANG']['MOD']['starrating_settings'][0] = 'Einstellungen';
