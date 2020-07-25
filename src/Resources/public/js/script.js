d126ef50e591d64d98b178d6a3b42f7f8e69a364
softleister <info@softleister.de>
2020-07-25 13:36:32 +0200

    Template- und Script-Anpassung

--- src/Resources/public/js/script.js
+++ src/Resources/public/js/script.js
@@ -12,26 +12,20 @@ $(function() {
             pageId = $form.find('input[name=pageID]').val()
             settingId = $form.find('input[name=settingID]').val();
 
-        var params = JSON.stringify({v: vote ,u: window.location.href, p: pageId, s: settingId});
+        // var params = JSON.stringify({v: vote ,u: window.location.href, p: pageId, s: settingId});
+        var params = JSON.stringify({v: vote, u: document.title.substring(0, document.title.lastIndexOf(" - ")), p: pageId, s: settingId});
 
         // Send the data using post
         var posting = $.post( url, { p: params } );
 
         // Put the results in a div
         posting.done(function( response ) {
-            // console.log($('.starability-result').data('rating'));
             if(response.new) {
                 $('.rating').hide();
                 $('.no-rating').show();
-                $('.average').text(response.average);
-                $('.starability-result').data('rating',response.average);
-                $('.rating-microdata').find('[itemprop=ratingValue]').prop('content',response.average);
-                $('.rating-microdata').find('[itemprop=worstRating]').prop('content',response.min);
-                $('.rating-microdata').find('[itemprop=bestRating]').prop('content',response.max);
-                $('.rating-microdata').find('[itemprop=ratingCount]').prop('content',response.count);
-
+                $('.average').text(Math.round(response.average.replace(",", ".")));
+                $('.starability-result').attr('data-rating', Math.round(response.average.replace(",", ".")));
             }
         });
     });
-    // console.log($('input[name=rating]'));
 });
